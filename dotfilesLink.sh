c871ca809445ec929e0194224ea3dde3d20c1b97
Ryuhei Yoshida <yoshida0112@chem.s.u-tokyo.ac.jp>
2022-01-11 12:24:46 +0900

    update dotfilesLink.sh

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,15 +1,40 @@
 #!/bin/bash
+
+# symbolic files under expand("~")
+ln -sf ~/dot_files/tmux/.tmux.conf ~/.tmux.conf
+ln -sf ~/dot_files/git_files/.gitconfig ~/.gitconfig
+ln -sf ~/dot_files/git_files/.gitattributes ~/.gitattributes
+ln -sf ~/dot_files/git_files/.gitignore_global ~/.gitignore_global
+
 mkdir -p ~/.config
 ln -sf ~/dot_files/fish ~/.config/
-
-mkdir -p ~/.config/git
-ln -sf ~/dot_files/git_files/config ~/.config/git/config
-ln -sf ~/dot_files/git_files/attributes ~/.config/git/attributes
-ln -sf ~/dot_files/git_files/gitignore_global ~/.config/git/gitignore_global
-
+ln -sf ~/dot_files/python/yapf ~/.config
 
 ln -sf ~/dot_files/vim/.vimrc ~/.vimrc
 mkdir -p ~/.config/vim/rc
 ln -sf ~/dot_files/vim/dein.toml ~/.config/vim/rc/dein.toml
 ln -sf ~/dot_files/vim/dein_lazy.toml ~/.config/vim/rc/dein_lazy.toml
+ln -sf ~/dot_files/vim/dein_lazy_ddc.toml ~/.config/vim/rc/dein_lazy_ddc.toml
 ln -sf ~/dot_files/vim/snippets ~/.config/vim/rc/
+
+
+ln -sf ~/dot_files/.latexmkrc ~/.latexmkrc
+
+if [ "$(uname)" = 'Darwin' ]; then
+	echo "mac"
+	## alias for man julia
+	ln -sf /Applications/Julia-*.app/Contents/Resources/julia/share/man/man1/* /usr/local/share/man/man1/
+	# see https://docs.docker.com/docker-for-mac/#fish-shell
+	ln -sf /Applications/Docker.app/Contents/Resources/etc/docker.fish-completion ~/.config/fish/completions/docker.fish
+	ln -sf /Applications/Docker.app/Contents/Resources/etc/docker-compose.fish-completion ~/.config/fish/completions/docker-compose.fish
+
+	## option to clangd
+	mkdir -p ~/Library/Preferences/clangd
+	ln -sf ~/dot_files/clangd/config.yaml ~/Library/Preferences/clangd/config.yaml
+elif [ "$(expr substr $(uname -s) 1 5)" = 'Linux' ]; then
+	echo "Linux"
+
+	## option to clangd
+	mkdir -p ~/.config/clangd
+	ln -sf ~/dot_files/clangd/config.yaml ~/.config/clangd/config.yaml.
+fi
